4e833d605af546922d547a614a7be86dc8c5b745
roin09 <roin14@gmail.com>
2023-03-07 22:49:09 +0900

    Fix: mainbox flex 수정

--- src/Pages/Main.jsx
+++ src/Pages/Main.jsx
@@ -89,8 +89,7 @@ const BgImage = styled.div`
 const MainBox = styled.div`
   display: flex;
   flex-direction: column;
-  width: 100%;
-  height: 100%;
+  align-items: center;
 `;
 const Container = styled.div`
   box-sizing: border-box;
