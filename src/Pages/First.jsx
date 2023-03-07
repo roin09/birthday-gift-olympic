c74ff1a6952c29e9bfc85d4d0350c4bfcf7ba4e8
roin09 <roin14@gmail.com>
2023-03-07 16:46:39 +0900

    Add: Swiper slide 기능 추가

--- src/Pages/First.jsx
+++ src/Pages/First.jsx
@@ -1,5 +1,13 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
+import { Swiper, SwiperSlide, useSwiperSlide, useSwiper } from "swiper/react";
+
+import "swiper/css";
+import "swiper/css/effect-coverflow";
+import "swiper/css/pagination";
+import "./first.css";
+// Swiper에서 가져올 모듈들
+import { EffectCoverflow, Pagination } from "swiper";
 
 import simplel1 from "./img/simplel1.png";
 import simplel2 from "./img/simplel2.png";
@@ -12,14 +20,15 @@ import simpleo2 from "./img/simpleo2.png";
 import simples1 from "./img/simples1.png";
 
 const First = () => {
-  const initialState = {
-    option: "first",
-  };
-  const [option, setOption] = useState(initialState);
+  const swiperSlide = useSwiperSlide();
+  const [swiper, setSwiper] = useState(null);
+
+  const [content, setContent] = useState(null);
   const handleClickButton = (e) => {
     const { name } = e.target;
-    setOption(name);
+    setContent(name);
   };
+
   // const dataList = {
   //   simples: ["simples1"],
   //   simplel: [
@@ -37,6 +46,7 @@ const First = () => {
   //   girlo: ["girlo1", "girlo2", "girlo3", "girlo4", "girlo5"],
   //   char: ["char1", "char2", "char3"],
   // };
+
   const simples = [simples1];
   const simplel = [
     simplel1,
@@ -50,47 +60,54 @@ const First = () => {
   const simpleo = [simpleo2];
   const SComponent = () => {
     return simples.map((data, idx) => (
-      <Content>
-        <img alt={idx} key={idx} src={data} />
-      </Content>
+      <SwiperSlide>
+        <img key={idx} alt={idx} src={data} />
+      </SwiperSlide>
     ));
   };
 
   const LComponent = () => {
     return simplel.map((data, idx) => (
-      <Content>
-        <img alt={idx} key={idx} src={data} />
-      </Content>
+      <SwiperSlide key={idx}>
+        <img alt={idx} src={data} />
+      </SwiperSlide>
     ));
   };
 
   const OComponent = () => {
     return simpleo.map((data, idx) => (
-      <Content>
-        {" "}
-        <img alt={idx} key={idx} src={data} />
-      </Content>
+      <SwiperSlide>
+        <img key={idx} alt={idx} src={data} />
+      </SwiperSlide>
     ));
   };
 
   const selectComponent = {
-    first: <SComponent />,
-    second: <LComponent />,
-    third: <OComponent />,
+    simples: [simples1],
+    simplel: [
+      simplel1,
+      simplel2,
+      simplel3,
+      simplel4,
+      simplel5,
+      simplel6,
+      simplel7,
+    ],
+    simpleo: [simpleo2],
   };
   const buttonData = [
     {
-      name: "first",
+      name: "simples",
       id: "f",
       text: "반팔",
     },
     {
-      name: "second",
+      name: "simplel",
       id: "s",
       text: "긴팔",
     },
     {
-      name: "third",
+      name: "simpleo",
       id: "o",
       text: "원피스",
     },
@@ -100,12 +117,48 @@ const First = () => {
       {data.text}
     </DefaultButton>
   ));
+  let [mySwiper, setMySwiper] = useState(null);
+  // useEffect(() => {
+  //   if (content) {
+  //     let mySwiper = new Swiper(".swiper-container", {
+  //       loop: true,
+  //       slidesPerView: "2",
+  //       centeredSlides: true,
+  //       pagination: {
+  //         el: ".swiper-pagination",
+  //         clickable: true,
+  //       },
+  //     });
+  //     setMySwiper(mySwiper);
+  //   }
+  // }, [content]);
   return (
     <>
-      <InnerBox>
-        <Container>{selectButtons}</Container>
-        <InnerContainer>{option && selectComponent[option]}</InnerContainer>
-      </InnerBox>
+      <Container>{selectButtons}</Container>
+
+      <Swiper
+        onSwiper={setSwiper}
+        effect={"coverflow"}
+        grabCursor={true}
+        centeredSlides={true}
+        slidesPerView={"auto"}
+        coverflowEffect={{
+          rotate: 50,
+          stretch: 0,
+          depth: 100,
+          modifier: 1,
+          slideShadows: true,
+        }}
+        pagination={true}
+        modules={[EffectCoverflow, Pagination]}
+        className="mySwiper"
+      >
+        {selectComponent[content]?.map((data, idx) => (
+          <SwiperSlide key={idx}>
+            <img key={idx} alt={idx} src={data} />
+          </SwiperSlide>
+        ))}
+      </Swiper>
     </>
   );
 };
@@ -143,12 +196,12 @@ const DefaultButton = styled.button`
     color: white;
   }
 `;
-const InnerContainer = styled.div`
-  display: flex;
-  flex-direction: row;
-  box-sizing: border-box;
-  background-color: rgba(255, 255, 255, 0.348);
-`;
+// const InnerContainer = styled.div`
+//   display: flex;
+//   flex-direction: row;
+//   box-sizing: border-box;
+//   background-color: rgba(255, 255, 255, 0.348);
+// `;
 const InnerBox = styled.div`
   display: flex;
   flex-direction: column;
